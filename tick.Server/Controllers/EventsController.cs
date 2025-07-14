f87c5dbcdca6aa9004459f12f7f80e4f6ff0069e
fth_msss <fatih.donmez2008@hotmail.com>
2025-07-14 16:09:47 +0300

    controller endpoints

---
+++ tick.Server/Controllers/EventsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using tick.Server.Models;
+
+namespace tick.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventsController : ControllerBase
+    {
+
+        AppDbContext _context;
+        public EventsController(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+
+
+       
+
+        [HttpGet("getEvents")]
+        public async Task<ActionResult<IEnumerable<Event>>> GetEvents()
+        {
+            var events = await _context.Event.ToListAsync();
+            return Ok(events);
+        }
+
+
+        /// <summary>
+        /// get physicalseatlayout from event table
+        /// get seats from the physicalseatlayout table
+        /// return seats
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        [HttpGet("getEventSeats")]
+        public async Task<ActionResult<IEnumerable<Seat>>> GetSeats() 
+        {
+            //1:how to do the query: get event from user, get seatlayout, get seats.
+            //2:also get tickets, get the event, get the layout, get seats.
+            //3: get seatlock, get event, get layout, get seats.
+            //query where 1+2= full seats, 1+3= busy seats, and the rest is avaible tickets 
+
+            var seats = await _context.Seat.ToListAsync();
+            return Ok(seats);
+        }
+    }
+}
---
+++ tick.Server/Controllers/LoginController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using tick.Server.Models;
+using tick.Server.Models.views;
+
+
+namespace tick.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class LoginController :ControllerBase
+    {
+        AppDbContext _context;
+        private readonly IConfiguration _config;
+        public LoginController(AppDbContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+
+        }
+
+
+
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserView dto)
+        {
+            //password check
+            var user = _context.User.SingleOrDefault(u => u.UserName == dto.UserName);
+            if (user == null || !BCrypt.Net.BCrypt.Verify(dto.Password, user.Password))
+            {
+                return Unauthorized("Invalid credentials");
+            }
+
+            // Create JWT
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config["Jwt:Secret"]!);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, user.UserName) }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwt = tokenHandler.WriteToken(token);
+
+            return Ok(new { token = jwt });
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] UserView dto)
+        {
+            if (_context.User.Any(u => u.UserName == dto.UserName))
+            {
+                return BadRequest("Email already in use.");
+            }
+
+            var user = new User
+            {
+                UserName = dto.UserName,
+                Password = BCrypt.Net.BCrypt.HashPassword(dto.Password)
+            };
+
+            _context.User.Add(user);
+            _context.SaveChanges();
+
+            return Ok("User registered.");
+        }
+
+        [HttpPost("createTicket")]
+        public IActionResult CreateTicket([FromBody] CreateTicketView dto)
+        {
+            //only call this after locking the ticket
+
+            //create the ticket
+            var ticket = new Ticket
+            {
+            Owner = dto.OwnerName,
+       
+            CreatedAt = DateTime.UtcNow,
+       
+            State = "Valid",
+      
+            EventId = dto.IdEvent,
+       
+            SeatId = dto.IdSeat
+            };
+
+           
+            _context.Ticket.Add(ticket);
+            _context.SaveChanges();
+
+            return Ok("Ticket created.");
+        }
+    }
+
+
+
+}
